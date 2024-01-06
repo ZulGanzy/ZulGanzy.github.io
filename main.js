f3e76e06bffcdd5819e3d468228a337644b1dd0a
ZulGanzy <zulskybot@gmail.com>
2024-01-06 10:27:02 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const day = new Date();
+const hari = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"];
+console.log(day);
+
+function ig() {
+  window.open("https://instagram.com/zullsaha", "_blank");
+}
+function github() {
+  window.open("https://github.com/zulganzy", "_blank");
+}
+function dc() {
+  window.open("https://discordapp.com/users/901682377254928414", "_blank");
+}
